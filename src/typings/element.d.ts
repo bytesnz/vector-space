cd8fc8f954166314eb8be7d5a8d9b243a9d4a177
Jack Farley <ls65kswk@bytes.nz>
2017-03-20 23:15:32 +0000

    Create element.d.ts
    Create basic object interfaces for elements of a vector space

---
+++ src/typings/element.d.ts
@@ -0,0 +1,72 @@
+declare namespace VectorSpace {
+  /// A reference to another object
+  export type Reference = any;
+  
+  export type Coordinate = [ number, number ];
+  
+  export type SnapPoint = 'tl' | 'tm' | 'tr' | 'mr' | 'br' | 'bm' | 'bl' | 'ml' | 'c';
+  
+  /// Element in VectorSpace's history
+  export interface Step {
+    /// Date element was created
+    created: Date,
+    /// User who created the element
+    user?: Reference,
+    /// Last time the element was created
+    lastModified?: Date,
+    /// Last user to modify the element
+    lastModifiedUser?: Reference
+  };
+  
+  /// Modification Step
+  export interface Modification extends Step {
+    /// the element modified
+    element: Reference
+  };
+  
+  /// Annotation Element
+  export interface Annotation extends Step {
+    /// Text of annotation
+    annotation: string,
+    /// Point that position will be relative to
+    snapPoint: SnapPoint,
+    /// Coordinates of Annotation relative to the 
+    coordinate: Coordinate,
+    /// Point coordinated relative to
+    relativePoint:
+  };  
+  
+  /// The base element of the objects in the VectorSpace
+  export interface Element extends Step {
+    /// Type of element
+    type: string,
+    /// Annotations on the element
+    annotations?: Annotation[]
+  };
+  
+  ///Point Element
+  export interface Point extends Element {
+    type: 'point',
+    point: Coordinate,
+    /// Size of point relative to the drawing unit
+    size?: number
+  };
+  
+  /// Line Element
+  export interface Line extends Element {
+    type: 'line',
+    /// Path of line
+    path: Coordinate[],
+    /// Thickness of line relative to the drawing unit
+    thickness?: number
+  };
+  
+  /// Image Element
+  export interface Image extends Element {
+    type: 'image',
+    /// Image reference
+    image: Reference,
+    /// Size of image in pixels
+    size: Coordinate
+  };
+}
